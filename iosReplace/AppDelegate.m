4fe71188077c9fa028183bc54237f8535829c6c5
无星 <329106954@qq.com>
2019-05-21 11:00:03 +0800

    1.更新wxbridge 2.新增rn-fetch-blob 3.优化脚本 4.新增appdelegate替换文件

---
+++ iosReplace/AppDelegate.m
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "AppDelegate.h"
+
+#import "WXRNViewCtrl.h"
+#import "WXTools.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [self setWindow];
+  [WXTools copyPackages];
+  return YES;
+}
+
+- (void)setWindow{
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  
+  WXRNViewCtrl *ctrl = [[WXRNViewCtrl alloc]initWithModuleName:@"baseDemo" fileName:@"baseDemo"];
+  
+  _nav = [[UINavigationController alloc]initWithRootViewController: ctrl];
+  _nav.navigationBarHidden = YES;
+  self.window.rootViewController = _nav;
+  [self.window makeKeyAndVisible];
+}
+
+@end
--- script/initiOS.sh
+++ script/initiOS.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+:<<BLOCK
 cd ./ios
 
 echo "查看下当前目录是否正常"
@@ -48,7 +49,7 @@ then
     #3. pod install
     pod install
     echo "一切都搞定了,开始运行iOS项目看下"
-:<<BLOCK
+#:<<BLOCK
     #4  绑定本地的默认library
     demoDir=$(cd ../ && pwd)
     echo "demo的地址$demoDir"
@@ -78,6 +79,109 @@ then
     echo "开始pod install"
     pod install
 BLOCK
-else
-    echo 'pod init失败,检查是否安装cocoapods!'
-fi
+# else
+#     echo 'pod init失败,检查是否安装cocoapods!'
+# fi
+
+
+#定义变量workpath标示当前脚本所处位置
+workpath="当前项目所处位置"
+targetName="当前项目名称"
+#获取当前项目所处位置
+function getPath(){
+    workpath=$(pwd)
+}
+# 获取当前项目名称
+#参数要求:1:workpath
+function getTargetName(){
+    targetName=${1##*/}
+    #echo "$1" # arguments are accessible through name, echo "$1" # arguments are accessible through , ,...,...
+}
+
+#安装link项目
+function npmlink(){
+    react-native link
+}
+#替换ios的appdelegate文件
+#参数要求:1:workpath 2:targetName
+function replaceAppdelegate(){
+    #1.定义新旧文件
+    newh=$1/iosReplace/AppDelegate.h
+    newm=$1/iosReplace/AppDelegate.m
+    oldh=$1/ios/$2/AppDelegate.h
+    oldm=$1/ios/$2/AppDelegate.m
+    #2.拷贝并替换文件
+    cp $newh $oldh
+    cp $newm $oldm
+    echo "替换完毕,删除待替换文件"
+    rm -r newm=$1/iosReplace
+    echo "删除成功"
+}
+
+#添加pod,
+#参数要求:1:workpath 2:targetName
+function addPod(){
+    cd $1/ios
+    pod init
+    if test -e ./Podfile
+    then
+        echo "当前项目名为:$2"
+
+        #1.删除不必要行
+        sed  -i "" "10,\$d"  ./Podfile
+
+        #2.在最后添加
+        replace_sentence="pod 'MBProgressHUD', '~> 1.1.0'"
+        replace_end="end"
+
+        #两次添加
+        sed -i "" "\$a\ 
+        $replace_sentence
+        "   ./Podfile
+
+        sed -i "" "\$a\ 
+        $replace_end
+        "   ./Podfile
+        #3. pod install
+        pod install
+        echo "一切都搞定了,开始运行iOS项目看下"
+    else
+        echo 'pod init失败,检查是否安装cocoapods!'
+    fi
+
+    # # sed用法进行替换
+    # # target 'demo' do
+    # #目标语句,
+    # target_sentence="target '$target_name' do"
+    # echo "目标语句$target_sentence"
+    # #替换内容
+    # #可用1
+    # # replace_sentence="$target_sentence \\\n  pod 'MBProgressHUD', '~> 1.1.0'"
+    # replace_sentence="pod 'MBProgressHUD', '~> 1.1.0'"
+    # #替换的目录
+    # #可用1
+    # # sed -i "" "s/$target_sentence/$replace_sentence/g" ./Podfile
+    # #可用2
+    # sed -i "" "/$target_sentence/a\ 
+    #     $replace_sentence
+    # "   ./Podfile
+
+    # echo "开始pod install"
+    # pod install
+}
+
+
+
+echo "1.获取当前项目路径"
+getPath
+echo "获取成功,路径$workpath"
+# targetName=${workpath##*/}
+getTargetName $workpath
+echo "2.当前项目名称:$targetName"
+echo "3.准备link项目"
+npmlink
+echo "link完毕,理论上没报错"
+echo "4.替换ios的启动文件"
+replaceAppdelegate $workpath $targetName
+echo "5.给ios加pod"
+addPod  $workpath $targetName
